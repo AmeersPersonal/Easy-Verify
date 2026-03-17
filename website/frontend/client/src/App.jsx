d5077dab5c34f27ed5e9006d6cdf6ed1962304c3
Tohny123 <68088010+Tohny123@users.noreply.github.com>
2026-03-16 20:49:48 -0400

    Updated frontend
    Updated frontend
    - Added Logo
    - Updated home page to reflect final design

--- website/frontend/client/src/App.jsx
+++ website/frontend/client/src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [clickText, setClickText] = useState("");
+
+
+  const test = () => {
+    setClickText("Verifying...");
+    
+  }
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
+        <img src= "/icon.png" width={400} height={400}/>
+        <h1>Access Content Below</h1> <br/>
+        <form action={test}>
+          <input/> <br/>
+          <button type="submit">Verify</button>
+        </form>
+
+
+        <h3>{clickText}</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
