7635802c1525c5c82520717846a0128aad745fd1
Ameer <71401181+AmeersPersonal@users.noreply.github.com>
2026-04-04 00:08:38 -0400

    fixed bugs relating to the age verifcation, added the dob text analyzer and a function to clean up temp photos

--- client/src/LLMs/main.py
+++ client/src/LLMs/main.py
@@ -1,9 +1,13 @@
 from deepface import DeepFace
 import cv2
-import easycor
+import easyocr
 import re
 from datetime import datetime
+import os
 
+# we should use this for when to store the photo of the person and when to delete it, we can also use it to store the photos of the ID for a short period of time
+PHOTO_DIR= os.path.dirname(os.path.realpath(__file__)) + "/temp_photos"
+ID_DIR = os.path.dirname(os.path.realpath(__file__)) + "/temp_ids"
 """
 I've decied this doesn't need OOP
 instead we will just utlize the methods in the client class
@@ -41,7 +45,7 @@ def estimate_age(img1, img2, img3)-> int:
     
     #averaged out age, should we automatically choose to lowest one?
 
-    final_guess = round(sum(estimate_age) / len(estimate_age))
+    final_guess = round(sum(estimated_age) / len(estimated_age))
 
     return final_guess
 
@@ -66,4 +70,36 @@ def verify_user_id(img1, img2)->bool:
 
 def extract_dob(img1):
     "This will extract DOB from an ID"
+    reader = easyocr.Reader(['en'], gpu=False) # lets assume the user does not have a gpu for now, we can add it later
+    result = reader.readtext(img1)
+    dob_pattern = r'(\d{2}[-/]\d{2}[-/]\d{4})|(\d{4}[-/]\d{2}[-/]\d{2})'
+    for res in result:
+        text = res[1]
+        match = re.search(dob_pattern, text)
+        if match:
+            dob_str = match.group(0)
+            try:
+                dob = datetime.strptime(dob_str, '%d-%m-%Y')
+            except ValueError:
+                try:
+                    dob = datetime.strptime(dob_str, '%Y-%m-%d')
+                except ValueError:
+                    continue
+            return dob.date()
+    return None
+
+
+def cleanup_temp_files():
+    """
+    This will clean up the temp photos and ids after we are done with them
+    should we implement this after running the functions or after we get the results back to the user?
+    """
     
+    for folder in [PHOTO_DIR, ID_DIR]:
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            except Exception as e:
+                print(f"Error deleting file {file_path}: {e}")
