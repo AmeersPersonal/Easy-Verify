7d70768f11e049bdd474c0eb0cf1918828f8449d
Jeffrey Chu <116784631+jeffreychu0@users.noreply.github.com>
2026-04-20 23:35:49 -0400

    Refactor verificationHandler to enable age estimation

--- client/src/util/verificationHandler.py
+++ client/src/util/verificationHandler.py
@@ -1,4 +1,4 @@
-# from LLMs.llm import estimate_age, FAILED_STATUS_CODE
+from LLMs.llm import estimate_age, FAILED_STATUS_CODE
 from util.webSocketHandler import finishVerify
 
 
@@ -7,16 +7,16 @@ from util.webSocketHandler import finishVerify
 
 def startVerification(img1, img2, img3, callUI):
     print("starting verification")
-    # result = 0
-    # try:
-    #     result = estimate_age(img1, img2, img3)
-    # except Exception as e:
-    #     print("ERROR")
-    #     print(e)
+    result = 0
+    try:
+        result = estimate_age(img1, img2, img3)
+    except Exception as e:
+        print("ERROR")
+        print(e)
 
-    # if result == FAILED_STATUS_CODE:
-    #     callUI.failedVerify()
-    #     print("Verification Error")
-    # else:
-    #     print(result)
-    finishVerify()  # maybe pass estimated age here
+    if result == FAILED_STATUS_CODE:
+        callUI.failedVerify()
+        print("Verification Error")
+    else:
+        print(result)
+        finishVerify()  # maybe pass estimated age here
