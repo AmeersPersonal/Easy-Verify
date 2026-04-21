bfabbb09fccfa16ac8a89f9bb46ec3be5109c94d
Ameer <71401181+AmeersPersonal@users.noreply.github.com>
2026-04-20 23:34:04 -0400

    bug fixes for llm and db

--- client/src/main.pyw
+++ client/src/main.pyw
@@ -1,8 +1,11 @@
 import sys, traceback
 from interface import mainUI
-
-
+from util.db.database import Database
+import time
 def main():
+    Database()  # initialize database and create tables if they don't exist
+    time.sleep(2)  #  waits till db is intiilzied
+    Database().close()  # close the connection since we don't need it right now, we will open it when we need to use it
     # The first command line arugument passes the entire URL that activated the application
     # so we can use that to trigger the application to open and do something with the URL data.
     print("URL:", sys.argv[1:])
--- client/src/util/db/database.py
+++ client/src/util/db/database.py
@@ -4,21 +4,23 @@ import sqlite3
 
 class Database:
     def __init__(self):
+        DB_PATH = os.path.join(os.path.dirname(__file__), "easyverify.db")
         self.conn = sqlite3.connect(DB_PATH)
         self.cursor = self.conn.cursor()
         self._create_tables()
-        DB_PATH = os.path.join(os.path.dirname(__file__), "easyverify.db")
+        
 
     def _create_tables(self):
         # company demo table
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS `companyx` (
-            `id` int(11) NOT NULL,
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT,
             `user_id` int(11) NOT NULL,
             `name` varchar(100) NOT NULL,
             `email` varchar(255) NOT NULL,
             `pw` char(100) NOT NULL,
             `verified` tinyint(1) DEFAULT NULL
+          
             );
 
         """)
@@ -26,13 +28,13 @@ class Database:
         # users table
         self.cursor.execute("""
             CREATE TABLE  IF NOT EXISTS `users` (
-            `id` int(11) NOT NULL,
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT,
             `username` varchar(100) NOT NULL,
             `email` varchar(255) NOT NULL,
             `pw` char(60) NOT NULL,
-            `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
             `is_persistent` tinyint(1) NOT NULL,
-            `last_login` timestamp NOT NULL DEFAULT current_timestamp()
+            `last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
             );
 
         """)
@@ -40,16 +42,16 @@ class Database:
         # verfication table only will be used to send if user is verified or not and then deleted unless they have the save feature enabled
         self.cursor.execute("""
             CREATE TABLE  IF NOT EXISTS `verifications` (
-            `id` int(11) NOT NULL,
-            `user_id` int(11) NOT NULL,
-            `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+            `user_id` INTEGER NOT NULL,
+            `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
             `result` tinyint(1) NOT NULL,
             `method` text DEFAULT NULL,
             `policy` text DEFAULT NULL,
             `confidence` int(11) DEFAULT NULL,
             `error_code` text DEFAULT NULL,
             `token` text NOT NULL,
-            `expires_at` timestamp NULL DEFAULT NULL
+            `expires_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
             );
         """)
         self.conn.commit()
@@ -67,13 +69,13 @@ class Database:
         return self.cursor.fetchone()[0]
 
     def validate_email(self, email) -> bool:
-        self.cursor.execute("SELECT id FROM users WHERE email = ?", (email,))
+        self.cursor.execute("SELECT id FROM users WHERE email = ?", (email))
         return self.cursor.fetchone() is not None
 
     # from company_table
     def is_verified_company(self, email):
         self.cursor.execute(
-            """ SELECT verified FROM companyx WHERE email = ? """, (email,)
+            """ SELECT verified FROM companyx WHERE email = ? """, (email)
         )
         result = self.cursor.fetchone()
         return result[0] == 1 if result else False
